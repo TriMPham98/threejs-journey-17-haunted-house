c8641de85ad103fa0ec2f212d8470a64b8260363
Tri <trimpham98@gmail.com>
2023-06-15 10:47:11 -0700

    Refactor ambient and moon light names in debug UI

--- src/script.js
+++ src/script.js
@@ -44,13 +44,13 @@ scene.add(floor);
  */
 // Ambient light
 const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
-gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001).name("Ambient Int.");
 scene.add(ambientLight);
 
 // Directional light
 const moonLight = new THREE.DirectionalLight("#ffffff", 0.5);
 moonLight.position.set(4, 5, -2);
-gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
+gui.add(moonLight, "intensity").min(0).max(1).step(0.001).name("Moon Light Int.");
 gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
