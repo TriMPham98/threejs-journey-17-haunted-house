a9083f5703755a2614499e2eee5f6c814b61fb65
Tri <trimpham98@gmail.com>
2023-06-15 11:42:08 -0700

    Use for loop to  add graves in a random circular pattern

--- src/script.js
+++ src/script.js
@@ -82,7 +82,16 @@ scene.add(graves);
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
 const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
 
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
 
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0, z);
+  graves.add(grave);
+}
 
 // Floor
 const floor = new THREE.Mesh(
