d9a9fd504f5c15b571b3b688fd27df91dd2409bd
Tri <trimpham98@gmail.com>
2023-06-15 11:46:02 -0700

    Randomize grave's y and z rotation

--- src/script.js
+++ src/script.js
@@ -82,14 +82,16 @@ scene.add(graves);
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
 const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 35; i++) {
   const angle = Math.random() * Math.PI * 2;
   const radius = 3 + Math.random() * 6;
   const x = Math.sin(angle) * radius;
   const z = Math.cos(angle) * radius;
 
   const grave = new THREE.Mesh(graveGeometry, graveMaterial);
-  grave.position.set(x, 0, z);
+  grave.position.set(x, 0.3, z);
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
   graves.add(grave);
 }
 
