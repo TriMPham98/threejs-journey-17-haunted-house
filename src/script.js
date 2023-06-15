82ee6f0226871e7c0522a78ad0ab5aa9665862f1
Tri <trimpham98@gmail.com>
2023-06-15 10:59:19 -0700

    Resize walls and translate y coordinate up by half the height

--- src/script.js
+++ src/script.js
@@ -29,9 +29,11 @@ scene.add(house);
 
 // Walls
 const walls = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.BoxGeometry(4, 2.5, 4),
   new THREE.MeshStandardMaterial({color: "#ac8e82"})
-  );
+);
+
+walls.position.y = 2.5 / 2;
 
 house.add(walls);
 
