699b95109754a9067a5e71f515ef7a271c6d2e44
Tri <trimpham98@gmail.com>
2023-06-15 11:30:10 -0700

    Add and translate bushes 3 and 4

--- src/script.js
+++ src/script.js
@@ -65,7 +65,15 @@ const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush2.scale.set(0.25, 0.25, 0.25);
 bush2.position.set(1.4, 0.1, 2.1);
 
-house.add(bush1, bush2);
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
 
 // Floor
 const floor = new THREE.Mesh(
