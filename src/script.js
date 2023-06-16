34718889707c8021ddad52c5142d19adb75cf252
Tri <trimpham98@gmail.com>
2023-06-16 10:57:26 -0700

    Allow objects to cast shadow

--- src/script.js
+++ src/script.js
@@ -164,6 +164,7 @@ for (let i = 0; i < 35; i++) {
   grave.position.set(x, 0.3, z);
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
   graves.add(grave);
 }
 
@@ -296,7 +297,23 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#262837");
+
+/**
+ * Shadows
+ */
 renderer.shadowMap.enabled = true;
+
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls.castShadow = true;
+bush1.castShadow = true;
+bush3.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
 
 /**
  * Animate
